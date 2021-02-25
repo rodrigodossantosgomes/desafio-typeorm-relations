8373d9c4416f707fc5a30ae41fc8185966f1f9f8
Rodrigo Gomes <dossantos.tb@gmail.com>
2021-02-24 22:25:17 -0300

    feat: constuido a modelagem das entidades

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,39 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const findProductByName = await this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return findProductByName;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    const findProductsById = await this.ormRepository.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+    return findProductsById;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    return this.ormRepository.save(products);
   }
 }
 
